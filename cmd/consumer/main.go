4ae354b2b8fc1110cb1b6ae6f6749cb1beac3fd6
Elias Fonseca <dev.eliasfonseca@outlook.com>
2022-12-23 20:51:52 -0300

    feat: added func worker and route total

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/EliasSantiago/app-rabbitmq/internal/order/infra/database"
@@ -29,7 +30,26 @@ func main() {
 	defer ch.Close()
 	out := make(chan amqp.Delivery)
 	go rabbitmq.Consume(ch, out)
-	for msg := range out {
+
+	qtdWorkers := 4
+	for i := 1; i <= qtdWorkers; i++ {
+		go worker(out, &uc, i)
+	}
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		getTotalUC := usecase.GetTotalUseCase{OrderRepository: repository}
+		total, err := getTotalUC.Execute()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		json.NewEncoder(w).Encode(total)
+	})
+	http.ListenAndServe(":8081", nil)
+}
+
+func worker(delivereMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerID int) {
+	for msg := range delivereMessage {
 		var inputDTO usecase.OrderInputDTO
 		err := json.Unmarshal(msg.Body, &inputDTO)
 		if err != nil {
@@ -40,7 +60,7 @@ func main() {
 			panic(err)
 		}
 		msg.Ack(false)
-		fmt.Println(outputDTO)
-		time.Sleep(500 * time.Microsecond)
+		fmt.Printf("Worker %d has processed order %s\n", workerID, outputDTO.ID)
+		time.Sleep(1 * time.Second)
 	}
 }
